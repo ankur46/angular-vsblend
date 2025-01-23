824aac1f497f73aa3f50a2f90ef6eda764cdfc6b
Ankur Deshwal <ankur.deshwal@yahooinc.com>
2025-01-23 15:49:57 +0530

    feat: added icons with selection

--- src/app/layout/layout/layout.component.ts
+++ src/app/layout/layout/layout.component.ts
@@ -1,17 +1,5 @@
 import { Component } from '@angular/core';
-import { FileNamePanelComponent } from '../../modules/file-name-panel/file-name-panel.component';
-import { IconPanelComponent } from '../../modules/icon-panel/icon-panel.component';
-import { MainViewComponent } from '../../modules/main-view/main-view.component';
-import { TerminalComponent } from '../../modules/terminal/terminal.component';
-import { TopSearchComponent } from '../../modules/top-search/top-search.component';
-
-const components = [
-  TopSearchComponent,
-  FileNamePanelComponent,
-  TerminalComponent,
-  MainViewComponent,
-  IconPanelComponent,
-];
+import { components } from './component.const';
 
 @Component({
   selector: 'app-layout',
